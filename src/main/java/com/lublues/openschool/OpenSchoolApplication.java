5ed9273e4408823d30695004e498d864f16fa234
lublues <luisdxl@gmail.com>
2022-09-25 09:41:37 -0300

    Init project

---
+++ src/main/java/com/lublues/openschool/OpenSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.lublues.openschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lublues/openschool/OpenSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lublues.openschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
